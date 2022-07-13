2a4b0ccf86fa7d57a95a8a329be6fb882e6c212d
ivan.guan <1142816971@qq.com>
2022-07-13 18:37:57 +0800

    chinese

---
+++ SSP_sharedmemtest.cpp
@@ -0,0 +1,243 @@
+/************************************************
+copyright:none
+filename: sharedmem_init.cpp
+description: shared memorary functions
+author: wumingghao
+version:v1.0
+*************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+#include "SSP_sharedmem.hpp"
+#include <unistd.h>
+#include <pthread.h>
+using namespace std;
+/*-------------------------------------------------------*/
+unsigned char str_rt[10];
+/************************************************
+function name :fl_SSP_InitDB
+description: initialize shared memorary of DataBase
+input: none
+output:none
+*************************************************/
+void fl_SSP_InitDB()
+{
+    int shm_id;
+
+    printf("create share memory size is %ld\r\n",SHM_DB_Size);
+    key_t key=ftok("/tmp",1);
+    printf("key:%d\r\n", key);
+//    key_t key = 0x01020001;
+    shm_id = shmget(key, SHM_DB_Size,IPC_CREAT|0666);        //creat
+    if(shm_id == -1)
+    {
+        printf("init create share memory fail!\r\n");
+        exit(1);
+    }
+}
+
+/************************************************
+function name :fg_SSPApp_GetDateFromDB
+description: get date frome shared memorary of DataBase
+input: signal--> name of signal      value
+output:none
+*************************************************/  
+struct SHM_DB_ST readptr1;
+struct SHM_DB_ST * fg_SSPApp_GetDateFromDB()
+{
+    int shm_id,flag;
+    
+    struct SHM_DB_ST *DataBase;
+    pthread_mutex_t * sharedLock;
+    struct shmid_ds buf ;
+    //char *DataBase;
+   
+    key_t key=ftok("/tmp",1);
+    shm_id = shmget(key, SHM_DB_Size,0);
+    if(shm_id == -1)
+    {
+        printf("create share memory fail!\r\n");
+        exit(1);
+    }
+    flag = shmctl( shm_id, IPC_STAT, &buf) ;
+    DataBase = (struct SHM_DB_ST *) shmat(shm_id, NULL, 0);
+    if(DataBase == (void*)-1)
+    {
+        printf("shmat err!\r\n");
+        exit(1);
+    }
+    //sharedLock = (pthread_mutex_t *)DataBase; 
+/*读数据前查看是否上锁*/    
+    //printf("read begain\r\n");
+    //pthread_mutex_lock(sharedLock);
+    //printf("read over\r\n");
+    memcpy(&readptr1, DataBase, SHM_DB_Size);
+    // readptr->count = DataBase->count;
+    // readptr->SignalArry[1].name = DataBase->SignalArry[1].name;
+/*读完数据解锁*/
+    //pthread_mutex_unlock(sharedLock);
+    shmdt(DataBase);
+    return &readptr1;
+} 
+// struct SHM_DB_ST * fg_SSPApp_GetDateFromDB()
+// {
+//     int shm_id;
+//     struct SHM_DB_ST *readptr;
+//     struct SHM_DB_ST *DataBase;
+//     pthread_mutex_t * sharedLock;
+//     key_t key=ftok("/tmp",1);
+//     shm_id = shmget(key, SHM_DB_Size,0);
+//     if(shm_id == -1)
+//     {
+//         printf("create share memory fail!\r\n");
+//         exit(1);
+//     }
+//     DataBase = (struct SHM_DB_ST *) shmat(shm_id, NULL, 0);
+//     if(DataBase == (void *)-1)
+//     {
+//         printf("shmat err!\r\n");
+//         exit(1);
+//     }
+// //     sharedLock = (pthread_mutex_t *)DataBase;  
+// // /*读数据前查看是否上锁*/
+// //     pthread_mutex_lock(sharedLock);  
+//     //ptr = *DataBase;
+//     // printf("read shared len %ld\r\n",SHM_DB_Size);
+//     // printf("11 %s\r\n",DataBase->SignalArry[0].name);
+//     // printf("11 %s\r\n",DataBase->SignalArry[1].name);
+//     memcpy(readptr, DataBase, SHM_DB_Size);
+//     printf("copy ok \r\n");
+
+// /*读完数据解锁*/
+//     //pthread_mutex_unlock(sharedLock);    
+//     if(shmdt(DataBase) < 0)
+//     {
+//         printf("shmdt err!\r\n");
+//         exit(1);        
+//     }
+//     printf("mdt end\r\n");
+//     //printf("11 %s\r\n",DataBase->SignalArry[1].name);
+//     //printf("sharedmem get signal %s : %s\r\n",&signal_name[signal][0], str.c_str());    
+//     return 0;
+// }  
+
+/************************************************
+function name :fg_SSPApp_SetDateToDBA
+description: get date frome shared memorary of DataBase
+input: signal--> name of signal      value
+output:none
+*************************************************/ 
+
+void fg_SSPApp_SetDateToDB(struct SHM_DB_ST* src)
+{
+    int shm_id;
+    struct SHM_DB_ST *DataBase;
+    pthread_mutex_t * sharedLock;
+	pthread_mutexattr_t ma;
+    key_t key=ftok("/tmp",1);
+    shm_id = shmget(key, SHM_DB_Size,0);
+    if(shm_id == -1)
+    {
+        printf("create share memory fail!\r\n");
+        exit(1);
+    }
+    DataBase = (struct SHM_DB_ST *) shmat(shm_id, NULL, 0);
+    if(DataBase == (void *)-1)
+    {
+        printf("shmat err!\r\n");
+        exit(1);
+    }
+//      sharedLock = (pthread_mutex_t *)DataBase;
+// /*初始化锁*/    
+//     pthread_mutexattr_init(&ma);
+//     pthread_mutexattr_setpshared(&ma, PTHREAD_PROCESS_SHARED);
+//     pthread_mutexattr_setrobust(&ma, PTHREAD_MUTEX_ROBUST);
+//     pthread_mutex_init(sharedLock,&ma);
+// /*写数据前上锁*/
+//      pthread_mutex_lock(sharedLock);
+    memcpy(DataBase, src, SHM_DB_Size);
+    //printf("sharedmem set signal %s : %s\r\n", &signal_name[signal][0], value);
+/*写完数据解锁*/    
+    
+    //pthread_mutex_unlock(sharedLock);
+    if(shmdt(DataBase) < 0)
+    {
+        printf("shmdt err!\r\n");
+        exit(1);        
+    }
+    
+}
+/************************************************
+function name :fg_SSPApp_RmDB
+description: remove sharedmemorary of DB__APP
+input: none
+output:none
+*************************************************/ 
+void fg_SSPApp_RmDB()
+{
+    int shm_id;
+    string str;
+
+    printf("rm share memory DBAPP\r\n");
+    key_t key=ftok("/tmp",1);
+    shm_id = shmget(key, SHM_DB_Size,0);
+    if(shm_id == -1)
+    {
+        printf("create share memory fail!\r\n");
+        exit(1);
+    }   
+    if (shmctl(shm_id, IPC_RMID, 0) == -1)  
+    {
+        printf("shmctl err!\r\n");
+    }
+    
+}    
+
+//int main()
+//{
+//    struct SHM_DB_ST DataBase;
+////  fl_SSP_InitDB();
+//    strcpy((char *)DataBase.SignalArry[1].name, "ESP");
+//    DataBase.SignalArry[1].enable = 1;
+//    DataBase.SignalArry[1].value_send = 49.01;
+//    DataBase.SignalArry[1].value_rcv = 50.02;
+//    DataBase.SignalArry[1].errrange = 0.1;
+//    fg_SSPApp_SetDateToDB(&DataBase);
+//    printf("success! %s\r\n",DataBase.SignalArry[1].name);
+//    //fg_SSPApp_RmDB();
+//    // DataBase = *fg_SSPApp_GetDateFromDB();
+//    // printf("get date %s\r\n",DataBase.SignalArry[1].name);
+//}
+
+int main()
+{
+    struct SHM_DB_ST DataBase;
+    printf("1\r\n");
+    fl_SSP_InitDB();
+    strcpy((char *)DataBase.SignalArry[1].name, "ESP");
+    DataBase.SignalArry[1].enable = 1;
+    DataBase.SignalArry[1].value_send = 49.01;
+    DataBase.SignalArry[1].value_rcv = 50.02;
+    DataBase.SignalArry[1].errrange = 0.1;
+    fg_SSPApp_SetDateToDB(&DataBase);
+
+    DataBase = *fg_SSPApp_GetDateFromDB();
+    printf("2\r\n");
+    printf("get date %s\r\n",DataBase.SignalArry[1].name);
+    printf("get date %d\r\n",DataBase.SignalArry[1].enable);
+    printf("get date %f\r\n",DataBase.SignalArry[1].value_send);
+    printf("get date %f\r\n",DataBase.SignalArry[1].value_rcv);
+    printf("get date %f\r\n",DataBase.SignalArry[1].errrange);
+    printf("get date %s\r\n",DataBase.SignalArry[2].name);
+    printf("get date %d\r\n",DataBase.SignalArry[2].enable);
+    printf("get date %f\r\n",DataBase.SignalArry[2].value_send);
+    printf("get date %f\r\n",DataBase.SignalArry[2].value_rcv);
+    printf("get date %f\r\n",DataBase.SignalArry[2].errrange);
+    //fg_SSPApp_RmDB();
+    printf("3\r\n");
+    sleep(1);
+    printf("4\r\n");
+    return 0;
+}
--- csv_test.py
+++ csv_test.py
@@ -22,7 +22,6 @@ def excel_trans(str, senddata, factor, offset):
         return 0
 
 #解析excel中FLOOR函数
-
 def FLOOR( a, b, c, d):
     tmp = a / b
     ans = (tmp - (tmp % c)) * d
@@ -70,27 +69,33 @@ def main():
     for j in range(FrameData.shape[1] - 11):
         # 遍历所有行
         for i in range(FrameData.shape[0]):
-            Factor = FrameData['Factor'][i]
+            Factor = FrameData['理论值factor'][i]
             Senddata = FrameData.iloc[i, 11 + j]
-            Offset = FrameData['Offset'][i]
+            Offset = FrameData['理论值offset'][i]
             DataBase.count = i
-            DataBase.SignalArry[i].enable = FrameData['0 Untest 1 Test'][i]
+            DataBase.SignalArry[i].enable = FrameData['0不测试 1测试'][i]
             DataBase.SignalArry[i].value_send = Senddata
             # print(FrameData['Theodata'][i])
-            DataBase.SignalArry[i].value_want = excel_trans(FrameData['Theodata'][i], Senddata, Factor, Offset)
+            DataBase.SignalArry[i].value_want = excel_trans(FrameData['MAF获取理论值'][i], Senddata, Factor, Offset)
 
             # DataBase.SignalArry[i].value_want = eval(FrameData['Theodata'][i])
-            DataBase.SignalArry[i].errrange = FrameData['errrange'][i]
-            DataBase.SignalArry[i].name = bytes(FrameData['Memsig'][i], encoding='utf-8')
-            print(j, "value_want:",DataBase.SignalArry[i].value_want)
+            DataBase.SignalArry[i].errrange = FrameData['允许误差%'][i]
+            DataBase.SignalArry[i].name = bytes(FrameData['共享内存内部信号'][i], encoding='utf-8')
+            # print(j, "value_want:",DataBase.SignalArry[i].value_want)
             # print(j,DataBase.count, DataBase.SignalArry[i].name, DataBase.SignalArry[i].enable,
             #       DataBase.SignalArry[i].value_send, "value_want:",DataBase.SignalArry[i].value_want, DataBase.SignalArry[i].errrange)
-
+            print(DataBase.count)
+            print(DataBase.SignalArry[i].enable)
+            print(DataBase.SignalArry[i].name)
+            print(DataBase.SignalArry[i].value_send)
+            print(DataBase.SignalArry[i].value_want)
+            print(DataBase.SignalArry[i].errrange)
         # 调用C接口
         # target.c_fg_SSPApp_SetDateToDB(byref(DataBase))
 
         # 延时
         time.sleep(1)
+
         # print(DataBase)
         # print(DataBase.count)
         # print(DataBase.SignalArry[i].enable)
